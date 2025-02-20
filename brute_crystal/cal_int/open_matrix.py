6eb32082e8713509352aefe7bb4d7565bc54f292
miru900 <miru405@gmail.com>
2025-02-20 21:32:21 +0900

    Delete brute_crystal directory

--- brute_crystal/cal_int/open_matrix.py
+++
@@ -1,6 +0,0 @@
-from cal_kim.open_grid import chem_compo as og_cc
-# importing file opening and symbol splitting function from cal_kim directory
-import numpy as np
-
-def mat_gen(pos_data, cell_size):
-    return np.array(pos_data) * cell_size * 1e-10
