1e3c642c54354a996c8da04c11791ce8e23d8453
miru900 <miru405@gmail.com>
2025-03-04 21:57:36 +0900

    Add files via upload

--- brute_crystal/cal_int/integer_program.py
+++ brute_crystal/cal_int/integer_program.py
@@ -3,14 +3,15 @@ from scipy.constants import elementary_charge as elec_c  ;  from scipy.constants
 from cal_int.pot_cal.elec_pot import energy_elec_addup as elad
 from cal_int.pot_cal.ewal_pot import energy_ewal_addup as ewad
 from cal_int.pot_cal.buck_pot import energy_buck_addup as buad
+from cal_int.pot_cal.buck_pot2 import energy_buck_addup as buad2
 import cal_int.constraints as cons
 
-def integer(chem, ion_count, matrix, info = None):
+def integer(chem, ion_count, ewal_mat = None, buck_mat = None, info = None):
     # example " chem = ["Sr", "Ti", "O"], ion_count = [1, 1, 3], grid = grid from open_grid, matrix = matrix from potential calculator
     # orb_key = orb_pos = [0, 1, 2, 3, 4, 5, 6, 7], orb_size = [1, 1, 1, 1, 1, 1, 1, 1], O = N (orbit length = grid square)
 
-    T = len(chem)  ;  O = len(matrix)
-    orb_key = [i for i in range(O)]  ;  orb_size = [1] * len(matrix)
+    T = len(chem)  ;  O = len(ewal_mat)
+    orb_key = [i for i in range(O)]  ;  orb_size = [1] * len(ewal_mat)
     
     # Model & Variables Setting
     m = gb.Model() ; Vars = [[] for i in range(T)]
@@ -36,18 +37,21 @@ def integer(chem, ion_count, matrix, info = None):
     # Making Objective Function, energy, using potential matrix generated from pot_cal
     energy1 = gb.QuadExpr()  ;  energy2 = gb.QuadExpr()
 
-    # Potential Addup, remove hashtag u want to calculate 
+    # Potential Addup, remove hashtag u want to calculate
+    ewad(O, T, Vars, orb_key, ewal_mat, charge, energy1, chem)
+    # buad(O, Vars, orb_key, energy2, chem, info[0], info[1], info[2])
+    buad2(O, Vars, orb_key, energy2, chem, buck_mat, info)
+
     # elad(O, T, Vars, orb_key, matrix, charge, energy1, chem)
-    ewad(O, T, Vars, orb_key, matrix, charge, energy1, chem)
     # buad(O, Vars, orb_key, energy2, chem, info[0], info[1], info[2])
 
     print("The objetive function was generated")
 
     # Special Constraints for Special Situation
-    cons.NCM_cons(m, Vars)
+    # cons.NCM_cons(m, Vars)
 
     # Setting Time limit, Solving QUBO problem, and return result
-    m.setParam("TimeLimit", 15)
+    m.setParam("TimeLimit", 600)
     m.setObjective(energy1 + energy2, gb.GRB.MINIMIZE)
     m.optimize()
     min_sym_pos = []
