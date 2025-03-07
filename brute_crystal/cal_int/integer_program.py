89bbe969b6163bfdb4aafe92b852fc224858aad9
miru900 <miru405@gmail.com>
2025-03-07 14:45:25 +0900

    Add files via upload

--- brute_crystal/cal_int/integer_program.py
+++ brute_crystal/cal_int/integer_program.py
@@ -4,9 +4,10 @@ from cal_int.pot_cal.elec_pot import energy_elec_addup as elad
 from cal_int.pot_cal.ewal_pot import energy_ewal_addup as ewad
 from cal_int.pot_cal.buck_pot import energy_buck_addup as buad
 from cal_int.pot_cal.buck_pot2 import energy_buck_addup as buad2
+from cal_int.pot_cal.ase_kim_pot import energy_kim_addup as kiad
 import cal_int.constraints as cons
 
-def integer(chem, ion_count, ewal_mat = None, buck_mat = None, info = None):
+def integer(chem, ion_count, mod, ewal_mat = None, buck_mat = None, kim_mat = None, info = None):
     # example " chem = ["Sr", "Ti", "O"], ion_count = [1, 1, 3], grid = grid from open_grid, matrix = matrix from potential calculator
     # orb_key = orb_pos = [0, 1, 2, 3, 4, 5, 6, 7], orb_size = [1, 1, 1, 1, 1, 1, 1, 1], O = N (orbit length = grid square)
 
@@ -30,29 +31,32 @@ def integer(chem, ion_count, ewal_mat = None, buck_mat = None, info = None):
             tmp.add(Vars[j][i], orb_size[i])
         m.addConstr(tmp == ion_count[j])
     
-    # add_extra_Cons(m, Vars)
-
     print("Variables and Constraints are generated")
 
-    # Making Objective Function, energy, using potential matrix generated from pot_cal
-    energy1 = gb.QuadExpr()  ;  energy2 = gb.QuadExpr()
-
     # Potential Addup, remove hashtag u want to calculate
-    ewad(O, T, Vars, orb_key, ewal_mat, charge, energy1, chem)
-    # buad(O, Vars, orb_key, energy2, chem, info[0], info[1], info[2])
-    buad2(O, Vars, orb_key, energy2, chem, buck_mat, info)
-
-    # elad(O, T, Vars, orb_key, matrix, charge, energy1, chem)
-    # buad(O, Vars, orb_key, energy2, chem, info[0], info[1], info[2])
+    
+    if mod == 4:
+        energy1, energy2 = mod_4(m, O, T, Vars, orb_key, ewal_mat, buck_mat, charge, chem, info)
+        e_sum = energy1 + energy2
+    if mod == 5:
+        energy1 = mod_5(m, O, T, Vars, orb_key, ewal_mat, charge, chem, info)
+        e_sum = energy1
+    if mod == 6:
+        energy1, energy2 = mod_6(m, O, T, Vars, orb_key, ewal_mat, buck_mat, charge, chem, info)
+        e_sum = energy1 + energy2
+    if mod == 7:
+        energy1, energy2 = mod_7(m, O, T, Vars, orb_key, ewal_mat, kim_mat, charge, chem, info)
+        e_sum = energy1 + energy2
 
     print("The objetive function was generated")
 
     # Special Constraints for Special Situation
-    # cons.NCM_cons(m, Vars)
+    # add_cons(m, Vars)
 
     # Setting Time limit, Solving QUBO problem, and return result
-    m.setParam("TimeLimit", 600)
-    m.setObjective(energy1 + energy2, gb.GRB.MINIMIZE)
+    m.setParam('NoRelHeurTime', 15)
+    m.setParam("TimeLimit", 60)
+    m.setObjective(e_sum, gb.GRB.MINIMIZE)
     m.optimize()
     min_sym_pos = []
 
@@ -64,3 +68,38 @@ def integer(chem, ion_count, ewal_mat = None, buck_mat = None, info = None):
     return min_sym_pos, m.objVal
 
 
+
+def mod_4(m, O, T, Vars, orb_key, ewal_mat, buck_mat, charge, chem, info):
+    energy1 = gb.QuadExpr()
+    ewad(O, T, Vars, orb_key, ewal_mat, charge, energy1, chem)
+    energy2 = gb.QuadExpr()
+    buad(O, Vars, orb_key, energy2, chem, info[0], info[1], info[2])
+    
+    return energy1, energy2
+
+def mod_5(m, O, T, Vars, orb_key, ewal_mat, charge, chem, info):
+    energy1 = gb.QuadExpr()
+    ewad(O, T, Vars, orb_key, ewal_mat, charge, energy1, chem)
+    
+    return energy1
+
+def mod_6(m, O, T, Vars, orb_key, ewal_mat, buck_mat, charge, chem, info):
+    energy1 = gb.QuadExpr()
+    ewad(O, T, Vars, orb_key, ewal_mat, charge, energy1, chem)
+    energy2 = gb.QuadExpr()
+    buad2(O, Vars, orb_key, energy2, chem, buck_mat, info)
+
+    return energy1, energy2
+
+def mod_7(m, O, T, Vars, orb_key, ewal_mat, kim_mat, charge, chem, info):
+    energy1 = gb.QuadExpr()
+    ewad(O, T, Vars, orb_key, ewal_mat, charge, energy1, chem)
+    energy2 = gb.QuadExpr()
+    kiad(O, Vars, orb_key, energy2, chem, kim_mat, info)
+
+    return energy1, energy2
+
+
+def add_cons(m, Vars):
+    print("Now adding Constraints")
+    cons.NCM_cons(m, Vars)
