84f3174d262044ac73d8d4f7657bdee322003570
miru900 <miru405@gmail.com>
2025-06-04 19:22:44 +0900

    Add files via upload

--- brute_crystal/settings.py
+++ brute_crystal/settings.py
@@ -6,7 +6,7 @@ settings = {
         "calc" : None, "angle" : None,
         "vari_range" : None},
     "NaCl_cubic" : {
-        "State" : 8, "Symbol" : "Na_2+Cl_2", "grid" : [2, 2, 2], "cell_size" : [5.64] * 3, "view" : True,
+        "State" : 0, "Symbol" : "Na_2+Cl_2", "grid" : [2, 2, 2], "cell_size" : [5.64] * 3, "view" : True,
         "calc" : "LJ_ElliottAkerson_2015_Universal__MO_959249795837_003", "angle" : None, "cons" : None,
         "vari_range" : 0.3},
     "SrTiO3_1" : {
@@ -18,8 +18,8 @@ settings = {
         "calc" : "LJ_ElliottAkerson_2015_Universal__MO_959249795837_003", "angle" : None,
         "vari_range" : 0.2},
     "LiCo_cathode" : {
-        "State" : 0, "Symbol" : "Li_3+Co_3+O_6", "grid" : [22, 52, 6], "cell_size" : [2.81126, 2.81126, 13.90946], "view" : True, # mp-22526 so C22526, for supercell, C22526 * supercell
-        "calc" : "LJ_ElliottAkerson_2015_Universal__MO_959249795837_003", "angle" : [90, 90, 120], "cons" : [[0, 1, 2], [3, 4, 5], [6, 7, 8, 9, 10, 11]],
+        "State" : 8, "Symbol" : "Li_12+Co_24+O_48", "grid" : [2025, 6, 5], "cell_size" : [2.81126 * 2, 2.81126 * 2, 13.90946 * 2], "view" : True, # mp-22526 so C22526, for supercell, C22526 * supercell
+        "calc" : "LJ_ElliottAkerson_2015_Universal__MO_959249795837_003", "angle" : [90, 90, 120], "cons" : [list(range(0, 24)), list(range(24, 48)) + ["Fixed"], list(range(48, 96)) + ["Fixed"]],
         "vari_range" : 0.2},
     "LiNiAl_Cathode" : {
         "State" : 0, "Symbol" : "Li_3+O_6+Ni_3", "grid" : [2019, 155, 18], "cell_size" : [2.86463, 2.86463, 14.23982], "view" : True, # Al3 doping, for bachelor's degree graduation paper
