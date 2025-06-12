f236bde3e47c5c280df2548747af553d8dbee6cb
miru900 <miru405@gmail.com>
2025-06-12 16:14:04 +0900

    Add files via upload

--- brute_crystal/cal_int/cons_file/constraints_Battest.py
+++ brute_crystal/cal_int/cons_file/constraints_Battest.py
@@ -1,10 +1,20 @@
 def LiCo(m, Vars):
-    for i in range(3, 12):
+    for i in range(24, 96):
         m.addConstr(Vars[0][i] == 0)
-    for i in range(6, 12):
+    for i in range(48, 96):
+        m.addConstr(Vars[2][i] == 1)
+    for i in range(24, 48):
         m.addConstr(Vars[1][i] == 1)
-    for i in range(0, 3):
-        m.addConstr(Vars[2][i] == 0)
-        m.addConstr(Vars[3][i] == 0)
-        m.addConstr(Vars[4][i] == 0)
+
+def NCM811(m, Vars):
+    for i in range(60, 240):
+        m.addConstr(Vars[0][i] == 0)
+    for i in range(60, 108):
+        m.addConstr(Vars[1][i] == 1)
+    for i in range(108, 114):
+        m.addConstr(Vars[2][i] == 1)
+    for i in range(114, 120):
+        m.addConstr(Vars[3][i] == 1)
+    for i in range(120, 240):
+        m.addConstr(Vars[4][i] == 1)
 
