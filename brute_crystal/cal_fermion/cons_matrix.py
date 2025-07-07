16d7bc4f6b71d77798f9747c9e590a0e4350320e
miru900 <miru405@gmail.com>
2025-07-07 19:23:22 +0900

    Update cons_matrix.py

--- brute_crystal/cal_fermion/cons_matrix.py
+++ brute_crystal/cal_fermion/cons_matrix.py
@@ -1,14 +1,17 @@
 import numpy as np
-
+  
 def matrix_cons_adder(W, cons, ion_count):
 
-    size = cons[0][-1]
+    size = len(cons[0])
 
     for i in range(size):
-        W[i, i] += 1 - 2 * ion_count[0]
+        idx = cons[i]
+        W[idx, idx] += 1 - 2 * ion_count[0]
         for j in range(i + 1, size):
-            W[i, j] += 2
+            jdx = cons[j]
+            W[idx, jdx] += 2
 
     return W
+
     
 
