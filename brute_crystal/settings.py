9c000e0ebeab4b4bd3596a782ea586d79e196f98
miru900 <miru405@gmail.com>
2025-07-15 16:30:07 +0900

    Add files via upload

--- brute_crystal/settings.py
+++ brute_crystal/settings.py
@@ -1,5 +1,7 @@
 import math
 
+cons_lico = [list(range(0, 24)), list(range(24, 48)) + ["Fixed"], list(range(48, 96)) + ["Fixed"]]
+
 settings = {
     "NaCl_square" : {
         "State" : 0, "Symbol" : "Na_2+Cl_2", "grid" : [17, 17, 17], "cell_size" : [5.64, 5.64, 5.64], "view" : True,
@@ -18,12 +20,14 @@ settings = {
         "calc" : "LJ_ElliottAkerson_2015_Universal__MO_959249795837_003", "angle" :[89.8172, 90.0286, 120.0661], "charge" : "NCM811",
         "vari_range" : 0.2},
     "LiCo_cathode" : {
-        "State" : 9, "Symbol" : "Li_12+Co_24+O_48", "grid" : [2025, 6, 5], "cell_size" : [2.81126 * 2, 2.81126 * 2, 13.90946 * 2], "view" : True, # mp-22526 so C22526, for supercell, C22526 * supercell
-        "calc" : "LJ_ElliottAkerson_2015_Universal__MO_959249795837_003", "angle" : [90, 90, 120], "cons" : [list(range(0, 24)), list(range(24, 48)) + ["Fixed"], list(range(48, 96)) + ["Fixed"]], "charge" : None,
+        "State" : 5, "Symbol" : "Li_12+Co_24+O_48", "grid" : [2025, 6, 5], "cell_size" : [2.81126 * 2, 2.81126 * 2, 13.90946 * 2], "view" : True, # mp-22526 so C22526, for supercell, C22526 * supercell
+        "calc" : "LJ_ElliottAkerson_2015_Universal__MO_959249795837_003", "angle" : [90, 90, 120], "cons" : None, "charge" : None,
         "vari_range" : 0.2},
     "LiNiAl_Cathode" : {
         "State" : 0, "Symbol" : "Li_3+O_6+Ni_3", "grid" : [2019, 155, 18], "cell_size" : [2.86463, 2.86463, 14.23982], "view" : True, # Al3 doping, for bachelor's degree graduation paper
         "calc" : None, "angle" : [90, 90, 120], "cons" : None, "charge" : None,
         "vari_range" : 0.2}
+    # 12 - 201915518, 96 - 202565, 48 - 202521091
+    # For fixing, + ["Fixed"], only first variables is variable, second and after are fixed.
 }
 
